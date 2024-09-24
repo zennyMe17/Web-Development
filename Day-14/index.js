d8451878673ab55d5743e5f2cdca6ae74b831b5a
zennyMe17 <gowda.hemanth.1718@gmail.com>
2024-09-24 17:11:32 +0530

    Fresh start

---
+++ Day-14/index.js
@@ -0,0 +1,44 @@
+// console.log('Nimma Namskara');
+// let name = ['Apple','mango',23];
+// let age = 17;
+// let status = (age>=18)?'yes':'no';
+// console.log(status);
+
+
+// let marks = 92;
+// let grade = '';
+
+// if(marks >= 90)
+//     grade = 'A';
+// else if(marks <= 89 && marks >= 80)
+//     grade = 'B';
+// else
+//     grade = 'F';
+
+// console.log(grade);
+
+// let num = 20;
+
+// switch(num){
+//     case 1:
+//         console.log(1);
+//         break;
+//     case 2:
+//         console.log(2);
+//         break;
+//     default:
+//         console.log('no number');
+// }
+
+
+// for(let i = 1;i<5;i++)
+//     {
+//         console.log(i);
+//     } 
+let i = 1;
+do
+{
+    console.log(i);
+    i++;
+}
+while(i<1);
